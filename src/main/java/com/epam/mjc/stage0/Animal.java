a3e3e862739a21a06612eb75ba12987f3e456310
Aleksei <alekseisligai@gmail.com>
2023-12-15 14:29:24 +0600

    Added inheritance, overrided methods from parent-class

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,25 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+    public  Animal (String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public Animal () {
+
+    }
+
+    public String getDescription() {
+        String pawS = numberOfPaws > 1? "s":"";
+        String fur = hasFur?"a":"no";
+        return String.format("This animal is mostly %s. It has %d paw%s and %s fur.", color, numberOfPaws, pawS, fur);
+
+    }
+
 
 }
